04bd92266c6e6ce5476d29ca88ff9cd81a2821ff
Josh L. Espinoza <jol.espinoz@gmail.com>
2026-03-10 12:04:40 -0700

    Fixed commands.leviathan-manifest.sh

--- test/commands/commands.leviathan-manifest.sh
+++ test/commands/commands.leviathan-manifest.sh
@@ -5,6 +5,6 @@ job_name="leviathan-manifest"
 working_directory="../working/${manifest_type}"
 reference_directory="${working_directory}/references"
 veba_directory="../../Analysis/veba_output/"
-manifest="${reference_directory}/manifest.${manifest_type}.tsv.gz"
+manifest="manifest.${manifest_type}.tsv.gz"
 compile-manifest-from-veba.py -i ${veba_directory} -t prokaryotic,eukaryotic -o ${manifest}
 
